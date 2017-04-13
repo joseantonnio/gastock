addee5220db1f4fdf45f310ae0dc9de6ee50bbfa
Alex Souza <souzzalex@gmail.com>
2017-04-12 21:06:53 -0300

    Inicio Caso de uso

---
+++ Gastock/src/gastock/Funcionario.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gastock;
+
+/**
+ *
+ * @author Alex
+ */
+public class Funcionario {
+
+    private int id;
+    private String nome;
+    private double salario;
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+       
+    }
+    
+    public void setSalario( double salario){
+        this.salario = salario;
+    }
+
+}
