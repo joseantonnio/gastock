d8e2ad6c27515aa8c68e0472e41645dfcd4696fb
José Antonio <jasj_brasil@hotmail.com>
2017-05-04 23:36:08 -0300

    Alterações importantes
    Foi alterada toda a estrutura de pacotes do projeto, bem como a meneira como o mesmo é inicializado.

---
+++ Gastock/src/br/com/ifspsaocarlos/gastock/controllers/Tanque.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifspsaocarlos.gastock.controllers;
+
+/**
+ *
+ * @author Alex
+ */
+public class Tanque {
+    
+    private double quantidade = 100;
+    private Combustivel combustivel;
+    private boolean status;
+    
+public boolean verificarQuantidade(double qtd){
+    
+   this.quantidade = this.quantidade - qtd;
+   
+   if (this.quantidade <= 100){
+       
+       //System.out.println("Nivel de estoque baixo");
+       return status = false;
+   }
+   
+   return status = true;
+   
+}
+
+    public double getQuantidade(){
+        return this.quantidade;
+    }
+    
+    public void abasteceVeiculo(double qtd){
+        this.quantidade -= qtd;
+    }
+    
+}
