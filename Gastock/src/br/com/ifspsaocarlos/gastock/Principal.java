4f629bbd4fbbae8cb5d3ed2f64b485a4ff5f8592
José Antonio <jasj_brasil@hotmail.com>
2017-05-10 20:43:32 -0300

    Codigo Splash

--- Gastock/src/br/com/ifspsaocarlos/gastock/Principal.java
+++ Gastock/src/br/com/ifspsaocarlos/gastock/Principal.java
@@ -5,10 +5,13 @@
  */
 package br.com.ifspsaocarlos.gastock;
 
-import java.util.Scanner;
-import br.com.ifspsaocarlos.gastock.views.desktop.Login;
-import br.com.ifspsaocarlos.gastock.views.embarcado.Painel;
 import br.com.ifspsaocarlos.gastock.views.splash.BemVindo;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.SplashScreen;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -16,46 +19,105 @@ import br.com.ifspsaocarlos.gastock.views.splash.BemVindo;
  */
 public class Principal {
 
+    static SplashScreen mySplash;
+    static Graphics2D splashGraphics;
+    static Rectangle2D.Double splashTextArea;
+    static Rectangle2D.Double splashProgressArea;
+    static Font font;
+
     public static void main(String args[]) {
+
+        splashInit();
+        appInit();
+
+        if (mySplash != null) {
+
+            mySplash.close();
+        }
 
         //Abertura por GUI
         BemVindo tela = new BemVindo();
         tela.setVisible(true);
         tela.setLocationRelativeTo(null);
-        /*
-        // Declara verificador de abertura
-        boolean abriu = false;
+    }
 
-        // Declara variavel de leitura
-        Scanner in = new Scanner(System.in);
+    private static void splashInit() {
 
-        while (!abriu) {
+        mySplash = SplashScreen.getSplashScreen();
 
-            System.out.println("Qual solução deseja executar? (embarcado|desktop)");
-            String opcao = in.next();
+        if (mySplash != null) {
 
-            switch (opcao) {
-                case "embarcado":
-                case "1":
-                    System.out.println("Abrindo sistema embarcado...");
-                    Painel embarcado = new Painel();
-                    abriu = true;
-                    embarcado.setVisible(true);
-                    break;
+            Dimension ssDim = mySplash.getSize();
+            int height = ssDim.height;
+            int width = ssDim.width;
 
-                case "desktop":
-                case "2":
-                    System.out.println("Abrindo sistema administrativo...");
-                    Login desktop = new Login();
-                    abriu = true;
-                    desktop.setVisible(true);
-                    break;
+            splashTextArea = new Rectangle2D.Double(15., height * 0.88, width * .45, 32.);
+            splashProgressArea = new Rectangle2D.Double(width * .55, height * .92, width * .4, 12);
 
-                default:
-                    System.out.println("Escolha uma das opções: embarcado (1) ou desktop (2).");
-                    break;
+            splashGraphics = mySplash.createGraphics();
+            font = new Font("Dialog", Font.PLAIN, 14);
+            splashGraphics.setFont(font);
+
+            splashText("Starting");
+            splashProgress(0);
+        }
+    }
+
+    public static void splashText(String str) {
+
+        if (mySplash != null && mySplash.isVisible()) {
+
+            splashGraphics.setPaint(Color.LIGHT_GRAY);
+            splashGraphics.fill(splashTextArea);
+
+            splashGraphics.setPaint(Color.BLACK);
+            splashGraphics.drawString(str, (int) (splashTextArea.getX() + 10), (int) (splashTextArea.getY() + 15));
+
+            mySplash.update();
+        }
+    }
+
+    public static void splashProgress(int pct) {
+
+        if (mySplash != null && mySplash.isVisible()) {
+
+            splashGraphics.setPaint(Color.LIGHT_GRAY);
+            splashGraphics.fill(splashProgressArea);
+
+            splashGraphics.setPaint(Color.BLUE);
+            splashGraphics.draw(splashProgressArea);
+
+            int x = (int) splashProgressArea.getMinX();
+            int y = (int) splashProgressArea.getMinY();
+            int wid = (int) splashProgressArea.getWidth();
+            int hgt = (int) splashProgressArea.getHeight();
+
+            int doneWidth = Math.round(pct * wid / 100.f);
+            doneWidth = Math.max(0, Math.min(doneWidth, wid - 1));
+
+            splashGraphics.setPaint(Color.BLUE);
+            splashGraphics.fillRect(x, y + 1, doneWidth, hgt - 1);
+
+            mySplash.update();
+        }
+    }
+
+    private static void appInit() {
+
+        for (int i = 1; i <= 10; i++) {
+
+            int pctDone = i * 10;
+            splashText("Carregando #" + i);
+            splashProgress(pctDone);
+
+            try {
+
+                Thread.sleep((long) (Math.random() * 1000));
+            } catch (InterruptedException ex) {
+
+                // ignore it
             }
-        }*/
+        }
     }
 
 }
